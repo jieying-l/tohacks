eb42e72d3a819e6d7564bfdc0c99898b231e2013
wzhang2705 <wz2705@gmail.com>
2020-05-03 04:15:22 -0400

    modified queue.js and store.js to implement queue

--- Queue.js
+++ Queue.js
@@ -1,12 +1,73 @@
-import React from 'react';
- 
-const Queue = () => {
-    return (
-       <div>
-          <h1>Queue</h1>
-           <p>Queue page body content</p>
-       </div>
-    );
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import Store from './Store.js';
+
+const textStyle = {
+    fontSize: 20
 }
- 
-export default Queue;
+const leftAlready = <h1 style={textStyle}>You already left the queue!</h1>
+const joinedAlready = <h1 style={textStyle}>You already joined the queue!</h1>
+
+export default class Queue extends Component {
+    constructor(props) {
+        super(props);
+        this.enqueue = this.enqueue.bind(this);
+        this.dequeue = this.dequeue.bind(this);
+        this.state = { quantity: 1,
+                        leftqueue: false, 
+                        joinedqueue: false};
+    }
+    
+    enqueue(props, state) { //increments queue number
+        if (this.state.leftqueue == true) {
+            return; //if the person has already left the queue, display a message indicating an error (display message leftAlready)
+        } else {
+            this.setState((state) => {
+                return {quantity: state.quantity + 1,
+                        leftqueue: true,
+                        joinedqueue: false
+                }
+            });
+        }   
+    }
+
+    dequeue(props, state) { //increments queue number
+        if (this.state.joinedqueue == true) {
+            return; //if the person has already joined the queue, display a message indicating an error (display message joinedAlready)
+        } else {
+            this.setState((state) => {
+                return {quantity: state.quantity - 1,
+                        leftqueue: false,
+                        joinedqueue: true
+                }
+            });
+        }   
+    }
+
+    render () {
+        return (
+        <div>
+            <h1>Costco Queue</h1>
+            <p>{this.state.quantity}</p>
+                <Link to='/Store'>
+            <button
+                className="btn btn-primary btn-lg"
+            >
+                Stores
+            </button>
+            </Link>
+            <button className="btn btn-primary btn-lg"
+                    onClick={this.enqueue} //calls this.enqueue
+            > 
+                Add to Queue
+            </button>
+            <button className="btn btn-primary btn-lg"
+                    onClick={this.dequeue} //calls this.dequeue
+            >
+                Leave Queue
+            </button>
+            
+        </div>
+        );
+    }
+}
--- Store.js
+++ Store.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import Queue from './Queue.js';
  
-const Store = () => {
+export default class Store extends Component {
+
+  render () {
     return (
        <div>
           <h1>Store</h1>
-           <p>Store page body content</p>
+           <p>Welcome to Costco!</p>
            
           <Link to='/Store/Queue'>
           <button
             className="btn btn-primary btn-lg"
+            
           >
-            Add to Queue
+            Visit Costco's Queue
           </button>
           </Link>
        </div>
     );
+  }
 }
- 
-export default Store;
